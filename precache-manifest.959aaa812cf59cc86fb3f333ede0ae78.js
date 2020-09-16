f3a64651b80fd947a416fa1ccbb307efea16d5a0
Dukhnich_M <maryna.dukhnich@gmail.com>
2020-09-16 21:20:11 +0300

    Updates

---
+++ precache-manifest.959aaa812cf59cc86fb3f333ede0ae78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a38b7b66e30f545809c3cb7e6f9f6a",
+    "url": "/reddit-search/index.html"
+  },
+  {
+    "revision": "b399aae1479a7dc35424",
+    "url": "/reddit-search/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5e691cb5a3035182ae96",
+    "url": "/reddit-search/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b399aae1479a7dc35424",
+    "url": "/reddit-search/static/js/2.5a404476.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reddit-search/static/js/2.5a404476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e691cb5a3035182ae96",
+    "url": "/reddit-search/static/js/main.97b6da4e.chunk.js"
+  },
+  {
+    "revision": "e67053e379773f5152e0",
+    "url": "/reddit-search/static/js/runtime-main.a9cda9eb.js"
+  }
+]);
